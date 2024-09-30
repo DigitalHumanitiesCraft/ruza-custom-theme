4109f3bc9393d3b131b31146bb461748372e0b90
Christian Steiner <ch.stnr@gmail.com>
2024-09-30 09:31:59 +0200

    Update audioplayer.js

--- asset/js/audioplayer.js
+++ asset/js/audioplayer.js
@@ -3,23 +3,22 @@ $(document).ready(function () {
   const $restoreIcon = $("#restoreIcon");
 
   // Minimize the window
-  $(".minimize-btn").on("click", function() {
+  $(".minimize-btn").on("click", function () {
     $audioPlayerWindow.hide(); // Hide the window
-    $restoreIcon.show();       // Show the restore icon
+    $restoreIcon.show(); // Show the restore icon
   });
 
   // Restore the window
-  $restoreIcon.on("click", function() {
+  $restoreIcon.on("click", function () {
     $audioPlayerWindow.show(); // Show the window
-    $restoreIcon.hide();       // Hide the restore icon
+    $restoreIcon.hide(); // Hide the restore icon
   });
 
   // Make the audio player window draggable by the header and resizable
-  $audioPlayerWindow
-    .draggable({
-      handle: ".window-header",
-      containment: "window", // This keeps the window within the browser viewport
-    })
+  $audioPlayerWindow.draggable({
+    handle: ".window-header",
+    containment: "window", // This keeps the window within the browser viewport
+  });
 
   // Open the audio player window when the play button in the carousel is clicked and play first song
   $(".carousel .play-button").on("click", function (e) {
